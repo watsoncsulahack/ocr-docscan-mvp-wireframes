f72bf9598670453dcacd4ee8bcb675ce8289c402
root <root@localhost.localdomain>
2026-04-13 23:18:12 +0000

    chore(control): show deployed revision metadata

--- control-panel/config.js
+++ control-panel/config.js
@@ -1,5 +1,5 @@
 // Control panel local config
 window.OCR_BACKEND_URL = "";
 window.OCR_FRONTEND_BASE_URL = "https://watsoncsulahack.github.io/ocr-docscan-mvp-wireframes/";
-window.OCR_CONTROL_REV = "947ea60+local";
-window.OCR_CONTROL_SOURCE = "uncommitted patches";
+window.OCR_CONTROL_REV = "7a19449";
+window.OCR_CONTROL_SOURCE = "github/main";
